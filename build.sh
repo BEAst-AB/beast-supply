b4b5781ba07897ecefdb8be877e0094824b777db
RikardLarsson-BEAst <rikard@beast.se>
2023-12-08 11:40:37 +0100

    Added new saxon processor for mapping documents

--- build.sh
+++ build.sh
@@ -113,31 +113,70 @@ docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgra
 
 # Generate mapping documents.
 echo "Generating mapping documents: Invoice"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-invoice.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/Invoice.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-invoice.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/Invoice.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Order"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-order.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/Order.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-order.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/Order.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Order response"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-order-response.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/OrderResponse.xml  -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-order-response.xml \
+    -xsl:/src/tools/create-mapping-document.xsl 
+    -o:/src/rules/mapping/OrderResponse.xml  -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Order agreement"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-orderagreement.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/Orderagreement.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-orderagreement.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/Orderagreement.xml -ext:on --allow-external-functions:on
 #echo ""Generating mapping documents: Catalogue"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-catalogue.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/Catalogue.xml  -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-catalogue.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/Catalogue.xml  -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Advanced Despatch advice"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-advanced-despatch-advice.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/AdvancedDespatchAdvice.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-advanced-despatch-advice.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/AdvancedDespatchAdvice.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Weight statement"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-weight-statement.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/WeightStatement.xml  -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-weight-statement.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/WeightStatement.xml  -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Transport execution plan request"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-transport-execution-plan-request.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/TransportExecutionPlanRequest.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-transport-execution-plan-request.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/TransportExecutionPlanRequest.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Transport execution plan"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-transport-execution-plan.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/TransportExecutionPlan.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-transport-execution-plan.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/TransportExecutionPlan.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Waybill"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-waybill.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/Waybill.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-waybill.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/Waybill.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Transportation Status Request"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-transportation-status-request.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/TransportationStatusRequest.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-transportation-status-request.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/TransportationStatusRequest.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Transportation Status"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-transportation-status.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/TransportationStatus.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-transportation-status.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/TransportationStatus.xml -ext:on --allow-external-functions:on
 echo "Generating mapping documents: Receipt Advice"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-receipt-advice.xml -xsl:/src/tools/create-mapping-document.xsl -o:/src/rules/mapping/ReceiptAdvice.xml -ext:on --allow-external-functions:on
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:/src/structure/syntax/ubl-receipt-advice.xml \
+    -xsl:/src/tools/create-mapping-document.xsl \
+    -o:/src/rules/mapping/ReceiptAdvice.xml -ext:on --allow-external-functions:on
 
 # Create examples based on documentation.
 echo "Generating example: Advanced Despatch advice"
@@ -147,19 +186,19 @@ echo "Generating example: Advanced Despatch advice"
     # -o:/src/rules/examples/AdvancedDespatchAdvice_Example_Full.xml \
     # overrideFile=src/structure/source/ubl-advanced-despatch-advice.xml -ext:on --allow-external-functions:on
 echo "Generating example: Weight statement"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target klakegg/saxon xslt -s:/src/structure/source/ubl-weight-statement.xml -xsl:/src/tools/create-example.xsl -o:/src/rules/examples/WeightStatement_Example_Full.xml  -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target klakegg/saxon xslt -s:/src/structure/source/ubl-weight-statement.xml -xsl:/src/tools/create-example.xsl -o:/src/rules/examples/WeightStatement_Example_Full.xml  -ext:on --allow-external-functions:on
 echo "Generating example: Transport execution plan request"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:latest -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transport-execution-plan-request.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportExecutionPlanRequest_Example_Full.xml -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:latest -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transport-execution-plan-request.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportExecutionPlanRequest_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Transport execution plan"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transport-execution-plan.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportExecutionPlan_Example_Full.xml -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transport-execution-plan.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportExecutionPlan_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Waybill"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-waybill.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/Waybill_Example_Full.xml -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-waybill.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/Waybill_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Transportation Status Request "
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transportation-status-request.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportationStatusRequest_Example_Full.xml -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transportation-status-request.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportationStatusRequest_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Transportation Status"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transportation-status.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportationStatus_Example_Full.xml -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-transportation-status.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/TransportationStatus_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Receipt Advice"
-docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-receipt-advice.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/ReceiptAdvice_Example_Full.xml  -ext:on --allow-external-functions:on
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.9.1-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/source/ubl-receipt-advice.xml -xsl:/src/tools/remove-pi.xsl -o:/src/rules/examples/ReceiptAdvice_Example_Full.xml  -ext:on --allow-external-functions:on
 
 # Structure
 docker run --rm -i \
