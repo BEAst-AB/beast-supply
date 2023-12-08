9a93e23ea6e19a194686df4ea96ec38e937bfa33
RikardLarsson-BEAst <rikard@beast.se>
2023-12-08 15:05:11 +0100

    2024

--- build.sh
+++ build.sh
@@ -187,44 +187,43 @@ docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgra
 # Create examples based on documentation.
 echo "Generating example: Advanced Despatch advice"
  docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
-     -s:/src/structure/source/ubl-advanced-despatch-advice.xml \
+     -s:/src/structure/syntax/ubl-advanced-despatch-advice.xml \
      -xsl:/src/tools/create-example.xsl \
      -o:/src/rules/examples/AdvancedDespatchAdvice_Example_Full.xml \
-     overrideFile=src/structure/source/ubl-advanced-despatch-advice.xml -ext:on --allow-external-functions:on
 echo "Generating example: Weight statement"
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-weight-statement.xml \
+    -s:/src/structure/syntax/ubl-weight-statement.xml \
     -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/WeightStatement_Example_Full.xml  -ext:on --allow-external-functions:on
 echo "Generating example: Transport execution plan request"
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-transport-execution-plan-request.xml \
-    -xsl:/src/tools/remove-pi.xsl \
+    -s:/src/structure/syntax/ubl-transport-execution-plan-request.xml \
+    -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/TransportExecutionPlanRequest_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Transport execution plan"
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-transport-execution-plan.xml \
-    -xsl:/src/tools/remove-pi.xsl \
+    -s:/src/structure/syntax/ubl-transport-execution-plan.xml \
+    -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/TransportExecutionPlan_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Waybill"
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-waybill.xml \
-    -xsl:/src/tools/remove-pi.xsl \
+    -s:/src/structure/syntax/ubl-waybill.xml \
+    -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/Waybill_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Transportation Status Request "
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-transportation-status-request.xml \
-    -xsl:/src/tools/remove-pi.xsl \
+    -s:/src/structure/syntax/ubl-transportation-status-request.xml \
+    -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/TransportationStatusRequest_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Transportation Status"
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-transportation-status.xml \
-    -xsl:/src/tools/remove-pi.xsl \
+    -s:/src/structure/syntax/ubl-transportation-status.xml \
+    -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/TransportationStatus_Example_Full.xml -ext:on --allow-external-functions:on
 echo "Generating example: Receipt Advice"
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform \
-    -s:/src/structure/source/ubl-receipt-advice.xml \
-    -xsl:/src/tools/remove-pi.xsl \
+    -s:/src/structure/syntax/ubl-receipt-advice.xml \
+    -xsl:/src/tools/create-example.xsl \
     -o:/src/rules/examples/ReceiptAdvice_Example_Full.xml  -ext:on --allow-external-functions:on
 
 
