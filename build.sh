7385f585f7b14317ee533a25c1e4c3df1cfc4002
Rikard Larsson <rikard@beast.se>
2024-12-06 15:57:11 +0100

    Adjustment of generation

--- build.sh
+++ build.sh
@@ -21,11 +21,8 @@ docker pull atomgraph/saxon
 
 # Transform the files in source dir to syntax.
 echo "Generating documentation: Invoice"
-docker run --rm -i \
-    -v $PROJECT:/src \
-    -v $PROJECT/target/generated:/target \
-    atomgraph/saxon \
-    -s:https://raw.githubusercontent.com/OpenPEPPOL/peppol-bis-invoice-3/refs/heads/2024-q4-release/structure/syntax/ubl-invoice.xml \ 
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target atomgraph/saxon \
+    -s:https://raw.githubusercontent.com/OpenPEPPOL/peppol-bis-invoice-3/2024-q4-release/structure/syntax/ubl-invoice.xml \ 
     -xsl:/src/tools/create-syntax.xsl \
     -o:/src/structure/syntax/ubl-invoice.xml \
     varOverrideSample=/src/structure/source/ubl-invoice.xml \
