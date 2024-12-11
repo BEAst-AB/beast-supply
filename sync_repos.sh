5f056625f807081232a2fbc8dae3f072661c2555
Rikard Larsson <rikard@beast.se>
2024-12-11 17:39:22 +0100

    Test of not commiting

--- sync_repos.sh
+++ sync_repos.sh
@@ -20,9 +20,9 @@ sync_repo "https://github.com/OpenPEPPOL/peppol-bis-invoice-3.git" "2024-q4-rele
 # Sync SCH Folder from OpenPEPPOL POACC Repository
 #git clone --depth=1 --branch 2024-q4-release https://github.com/OpenPEPPOL/poacc-upgrade-3.git source-repo
 #cp -r source-repo/rules/sch ./rules/sch # Copy SCH folder to the target path
-git config user.name "GitHub Actions"
-git config user.email "actions@github.com"
-git add rules/sch
-git commit -m "Sync SCH folder from OpenPEPPOL repository"
+#git config user.name "GitHub Actions"
+#git config user.email "actions@github.com"
+#git add rules/sch
+#git commit -m "Sync SCH folder from OpenPEPPOL repository"
 
 # Sync SCH Folder from OpenPeppol Billing Repository
