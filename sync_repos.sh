8e909d2225e3f152c7e1d894f3dbbf3c1f4e20df
Rikard Larsson <rikard@beast.se>
2024-12-11 13:36:57 +0100

    Sync of sch fodler from POACC

---
+++ sync_repos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Sync SCH Folder from OpenPEPPOL Repository
+git clone --depth=1 --branch 2024-q4-release https://github.com/OpenPEPPOL/poacc-upgrade-3.git source-repo
+cp -r source-repo/rules/sch rules/sch # Copy SCH folder to the target path
+git config user.name "GitHub Actions"
+git config user.email "actions@github.com"
+git add rules/sch
+git commit -m "Sync SCH folder from OpenPEPPOL repository"
