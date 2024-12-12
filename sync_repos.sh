e307a8e383bb78d93310ed07587f4a5a36024609
Rikard Larsson <rikard@beast.se>
2024-12-12 10:05:55 +0100

    Bugfix to get rid of sch/sch

--- sync_repos.sh
+++ sync_repos.sh
@@ -8,21 +8,14 @@ sync_repo() {
   local target_path=$4
 
   git clone --depth=1 --branch "$branch" "$repo_url" temp-repo
-  cp -r temp-repo/"$source_path" "$target_path"
+  # Ensure the target directory exists
+  mkdir -p "$target_path"
+  # Copy contents of the source directory to the target directory
+  cp -r temp-repo/"$source_path"/* "$target_path"
   rm -rf temp-repo # Clean up temporary repo
 }
 
-# Sync files from multiple repositories
-sync_repo "https://github.com/OpenPEPPOL/poacc-upgrade-3.git" "2024-q4-release" "rules/sch" "./rules/sch"
-sync_repo "https://github.com/OpenPEPPOL/peppol-bis-invoice-3.git" "2024-q4-release" "rules/sch" "./rules/sch"
+# Sync files from multiple repositories without './' in target path
+sync_repo "https://github.com/OpenPEPPOL/poacc-upgrade-3.git" "2024-q4-release" "rules/sch" "rules/sch"
+sync_repo "https://github.com/OpenPEPPOL/peppol-bis-invoice-3.git" "2024-q4-release" "rules/sch" "rules/sch"
 
-
-# Sync SCH Folder from OpenPEPPOL POACC Repository
-#git clone --depth=1 --branch 2024-q4-release https://github.com/OpenPEPPOL/poacc-upgrade-3.git source-repo
-#cp -r source-repo/rules/sch ./rules/sch # Copy SCH folder to the target path
-#git config user.name "GitHub Actions"
-#git config user.email "actions@github.com"
-#git add rules/sch
-#git commit -m "Sync SCH folder from OpenPEPPOL repository"
-
-# Sync SCH Folder from OpenPeppol Billing Repository
