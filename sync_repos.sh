e9b4e6b9e643951b550ff1eff6735aa4c9912453
Rikard Larsson <rikard@beast.se>
2025-01-16 17:29:35 +0100

    Sync profiles from Logistics and Poacc

--- sync_repos.sh
+++ sync_repos.sh
@@ -37,7 +37,20 @@ sync_repo $POACCREPO $POACCBRANCH "rules/examples/Cataloge wo response use cases
 sync_repo $POACCREPO $POACCBRANCH "rules/examples/Order use cases" "rules/examples/Order use cases"
 sync_repo $POACCREPO $POACCBRANCH "rules/examples/Order-response use cases" "rules/examples/Order-response use cases"
 sync_repo $LOGISTICSREPO $LOGISTICSBRANCH  "rules/examples" "rules/examples"
+
 # Code lists
 sync_repo $BILLINGREPO $BILLINGBRANCH "structure/codelist" "structure/codelist"
 sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "structure/codelist" "structure/codelist"
 sync_repo $POACCREPO $POACCBRANCH "structure/codelist" "structure/codelist"
+
+# Code lists
+sync_repo $POACCREPO $POACCBRANCH "guides/profiles/1-catalogueonly" "guides/profiles/1-catalogueonly"
+sync_repo $POACCREPO $POACCBRANCH "guides/profiles/64-catalogue-wo-response" "guides/profiles/64-catalogue-wo-response"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/66-advanceddespatchadvice" "guides/profiles/66-advanceddespatchadvice"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/67-advanceddespatchadvice_w_response" "guides/profiles/67-advanceddespatchadvice_w_response"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/68-weightstatement" "guides/profiles/68-weightstatement"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/69-transportexecutionplan_w_request" "guides/profiles/69-transportexecutionplan_w_request"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/70-transportexecutionplan" "guides/profiles/70-transportexecutionplan"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/71-waybill" "guides/profiles/71-waybill"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/72-transportationstatus_w_request" "guides/profiles/72-transportationstatus_w_request"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "guides/profiles/73-transportationstatus" "guides/profiles/73-transportationstatus"
