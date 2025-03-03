244e7a9421823af5b21ae2ca64b070051cd8568e
RikardLarsson-BEAst <rikard@beast.se>
2025-03-03 17:38:59 +0100

    used correct branch names

--- sync_repos.sh
+++ sync_repos.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 BILLINGREPO="https://github.com/OpenPEPPOL/peppol-bis-invoice-3.git"
-BILLINGBRANCH="main"
+BILLINGBRANCH="master"
 LOGISTICSREPO="https://github.com/OpenPEPPOL/Logistics-bis.git"
 LOGISTICSBRANCH="main"
 POACCREPO="https://github.com/OpenPEPPOL/poacc-upgrade-3.git"
-POACCBRANCH="main"
+POACCBRANCH="master"
 
 
 
