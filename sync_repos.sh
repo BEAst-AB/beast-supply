f373098995e18a5e85ab0d8b7651d62f31b61f6e
oscarglanzmann <oscar.glanzmann@ecru.se>
2025-05-26 16:43:56 +0200

    fix

--- sync_repos.sh
+++ sync_repos.sh
@@ -29,8 +29,9 @@ sync_repo() {
 # Rules
 sync_repo $BILLINGREPO $BILLINGBRANCH "rules/sch" "rules/sch"
 sync_repo $BILLINGREPO $BILLINGBRANCH "structure/syntax/part" "structure/syntax/part"
-sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "rules/sch" "rules/sch"
 sync_repo $POACCREPO $POACCBRANCH "rules/sch" "rules/sch"
+sync_repo $LOGISTICSREPO $LOGISTICSBRANCH "rules/sch" "rules/sch"
+
 
 # Examples
 sync_repo $POACCREPO $POACCBRANCH "rules/examples/Cataloge wo response use cases" "rules/examples/Cataloge wo response use cases"
