efea210c2767f3713522adcd71506fb634560613
Rikard Larsson <rikard@beast.se>
2025-11-04 17:07:47 +0100

    Added adoc to commit

--- build.sh
+++ build.sh
@@ -297,3 +297,7 @@ docker run --rm -i -v $PROJECT:/documents -v $PROJECT/target:/target difi/asciid
 
 echo "Fix ownership"
 docker run --rm -i -v $PROJECT:/src alpine:3.6 chown -R $(id -g $USER).$(id -g $USER) /src/target
+
+# Remove target directory
+echo "Removing target directory"
+docker run --rm -i -v $PROJECT:/src alpine:3.6 rm -rf /src/target
