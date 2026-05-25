d1f189073480469a75de216d6c4b4006e19d1b55
oscarglanzmann <oscar.glanzmann@ecru.se>
2026-05-25 10:21:50 +0200

    fix validation script version

--- build.sh
+++ build.sh
@@ -272,7 +272,7 @@ docker run --rm -i \
 
 echo "Testing validation rules"
 # old: docker run --rm -i -v $PROJECT:/src anskaffelser/validator:2.1.0 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
-docker run --rm -i -v $PROJECT:/src phelger/vefa-validator:2.3.1 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
+docker run --rm -i -v $PROJECT:/src phelger/vefa-validator:2.4.3 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
 
 # Removing old zip files and creating new ones
 docker run --rm -i \
