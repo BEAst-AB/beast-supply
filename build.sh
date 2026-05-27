db1db9402c51e2765455f2f89d6e6ae50d53b178
oscarglanzmann <oscar.glanzmann@ecru.se>
2026-05-27 08:21:54 +0200

    another try

--- build.sh
+++ build.sh
@@ -273,7 +273,7 @@ docker run --rm -i \
 echo "Testing validation rules"
 # old: docker run --rm -i -v $PROJECT:/src anskaffelser/validator:2.1.0 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
 #docker run --rm -i -v $PROJECT:/src phelger/vefa-validator:2.4.3 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
-docker run --rm -i -e "LOG4J_LOGGER_COM_HELGER=WARN" -v $PROJECT:/src phelger/vefa-validator:2.4.3 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
+docker run --rm -i -e "JAVA_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=warn" -v $PROJECT:/src phelger/vefa-validator:2.4.3 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
 # Removing old zip files and creating new ones
 docker run --rm -i \
   -v $PROJECT/target/site/files:/files \
